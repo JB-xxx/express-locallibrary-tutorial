c574326b51322fb81f951558e134133db22b0c48
Jan Baars <janbaars@mbp-van-jan.home>
2020-02-13 18:07:21 +0100

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Mijnvoorraad12:Devoorraad12@voorraadcluster-qwgrl.azure.mongodb.net/mijn_voorraad';
+
+var dev_db_url = 'mongodb+srv://Mijnvoorraad12:Devoorraad12@voorraadcluster-qwgrl.azure.mongodb.net/mijn_voorraad';
+// var mongoDB = 'mongodb+srv://Mijnvoorraad12:Devoorraad12@voorraadcluster-qwgrl.azure.mongodb.net/mijn_voorraad';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
